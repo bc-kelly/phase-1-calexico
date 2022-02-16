4a69e9f7fec7d57c0bdc7a9c9b5f72269806719d
Bridget Kelly <bckelly197@gmail.com>
2022-02-16 16:42:53 -0500

    challenge 3 done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Write your code here...
-//DONE Fetch all the menu items from http://localhost:3000/menu. For each menu item create a span element that contains the name of the menu item, and add it to the #menu-items div.
-// When the page loads, display the first menu item. You should set the image, name, description, and price. All the correct elements to set are located in the #dish section element.
+//DONE --Fetch all the menu items from http://localhost:3000/menu. For each menu item create a span element that contains the name of the menu item, and add it to the #menu-items div.
+//DONE-- When the page loads, display the first menu item. You should set the image, name, description, and price. All the correct elements to set are located in the #dish section element.
+//DONE -- When the user clicks on the menu items on the left, they should see all the details for that specific menu item.
 
 let menus;
 fetch('http://localhost:3000/menu')
@@ -20,6 +21,11 @@ function addToMenu (allItems) {
     let foodNames = document.createElement('span')
     foodNames.textContent = allItems.name
     menuList.append(foodNames);
+
+    foodNames.addEventListener('click', e => {
+        console.log('clicked')
+        showMenuItem(allItems)
+    })
 }
 
 function showMenuItem (dishItem) {
